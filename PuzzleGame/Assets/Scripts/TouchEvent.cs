e295c1f76f1aba2fe99b6eef15f140f14f4c3e08
manlok1992 <manlok@Mans-Mac-mini-2.local>
2014-12-19 19:21:40 +0800

    update

--- PuzzleGame/Assets/Scripts/TouchEvent.cs
+++ PuzzleGame/Assets/Scripts/TouchEvent.cs
@@ -4,38 +4,41 @@ using System.Collections;
 public class TouchEvent : MonoBehaviour {
 	// Use this for initialization
 	Vector2 beginPos;
+	bool inUp = false;
 	void Start () {
 	
 	}
 	
 	// Update is called once per frame
 	void Update () {
-
+		if(inUp) {
+			if(Input.GetAxis("Mouse X") < -0.3){
+				//Code for action on mouse moving left
+				print("Mouse moved left");
+			}
+			if(Input.GetAxis("Mouse X") > 0.3){
+				//Code for action on mouse moving right
+				print("Mouse moved right");
+			}
+			if(Input.GetAxis("Mouse Y") < -0.3){
+				//Code for action on mouse moving down
+				print("Mouse moved down");
+			}
+			if(Input.GetAxis("Mouse Y") > 0.3){
+				//Code for action on mouse moving up
+				print("Mouse moved up");
+			}
+		}
 	}
 
 	void OnMouseDown() {
 		beginPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+		inUp = true;
 	}
 
 	void OnMouseUp() {
 		float dirX = Camera.main.ScreenToWorldPoint(Input.mousePosition).x - beginPos.x;
 		float dirY = Camera.main.ScreenToWorldPoint(Input.mousePosition).y - beginPos.y;
-		if(Input.GetAxis("Mouse X") < 0.3){
-			//Code for action on mouse moving left
-			print("Mouse moved left");
-		}
-		if(Input.GetAxis("Mouse X") > 0.3){
-			//Code for action on mouse moving right
-			print("Mouse moved right");
-		}
-		if(Input.GetAxis("Mouse Y") < 0.3){
-			//Code for action on mouse moving down
-			print("Mouse moved down");
-		}
-		if(Input.GetAxis("Mouse Y") > 0.3){
-			//Code for action on mouse moving up
-			print("Mouse moved up");
-		}
-		Debug.Log ("X = "+dirX+" Y = "+dirY);
+		inUp = false;
 	}
 }
