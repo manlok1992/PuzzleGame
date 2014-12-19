b24754c1d3050be8386e84d03876833efea4de9a
manlok1992 <manlok@Mans-Mac-mini-2.local>
2014-12-19 18:46:35 +0800

    update

---
+++ PuzzleGame/Assets/Scripts/CreatePuzzle.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CreatePuzzle : MonoBehaviour {
+	public int num = 8;
+	public Object[,] obj;
+	public int[,] randIndex;
+	public GameObject[] prefab;
+
+	// Use this for initialization
+	void Start () {
+		randIndex = new int[num,num];
+		for(int i = 0; i < num; i++) {
+			for(int j = 0; j < num; j++) {
+				randIndex[i,j] = UnityEngine.Random.Range(0,4);
+			}
+		}
+		obj = new GameObject[num,num];
+		for(int i = 0; i < num; i++) {
+			for(int j = 0; j < num; j++) {
+				obj[i,j] = GameObject.Instantiate(prefab[randIndex[i,j]], new Vector3(-3+(1*i), -3+(1*j), 0), Quaternion.identity);
+			}
+		}
+		foreach(GameObject o in obj) {
+			o.AddComponent("TouchEvent");
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+}
---
+++ PuzzleGame/Assets/Scripts/TouchEvent.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TouchEvent : MonoBehaviour {
+	// Use this for initialization
+	Vector2 beginPos;
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+	void OnMouseDown() {
+		beginPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+	}
+
+	void OnMouseUp() {
+		float dirX = Camera.main.ScreenToWorldPoint(Input.mousePosition).x - beginPos.x;
+		float dirY = Camera.main.ScreenToWorldPoint(Input.mousePosition).y - beginPos.y;
+		if(Input.GetAxis("Mouse X") < 0.3){
+			//Code for action on mouse moving left
+			print("Mouse moved left");
+		}
+		if(Input.GetAxis("Mouse X") > 0.3){
+			//Code for action on mouse moving right
+			print("Mouse moved right");
+		}
+		if(Input.GetAxis("Mouse Y") < 0.3){
+			//Code for action on mouse moving down
+			print("Mouse moved down");
+		}
+		if(Input.GetAxis("Mouse Y") > 0.3){
+			//Code for action on mouse moving up
+			print("Mouse moved up");
+		}
+		Debug.Log ("X = "+dirX+" Y = "+dirY);
+	}
+}
